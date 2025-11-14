6737290a20b20393c13067d04e4cbf2e4834936f
Jesus De la Cruz <jose.delacruzsnt@uanl.edu.mx>
2025-11-13 19:26:29 -0600

    Backend en NetCore

---
+++ Backend-NetCore/ExamenJoseDelaCruz/ExamenJoseDelaCruz/Program.cs
@@ -0,0 +1,42 @@
+using Examen.DataAccess;
+using Examen.Business;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ILogger, FileLogger>();
+builder.Services.AddHttpClient<IJsonPlaceholderService, JsonPlaceholderService>(client =>
+{
+    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
+
+app.MapControllers();
+
+app.Run();
